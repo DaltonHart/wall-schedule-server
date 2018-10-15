0c70bcdc52f2e2af9c3ae30b765f6bbd60e46450
DaltonHart <daltonvaren@gmail.com>
2018-10-15 16:19:19 -0700

    added bart information

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,36 @@
 console.log('Sanity. I has it.')
+
+let bartApi = `http://api.bart.gov/api/etd.aspx?cmd=etd&orig=dubl&key=MW9S-E7SL-26DU-VV8V&json=y`
+
+
+const bartSucc = (res) => {
+    let station = res.root.station[0]
+    let departures = station.etd[0].estimate
+    $('#depart').empty()
+    $('#bart').empty()
+    console.log(station)
+    console.log(departures)
+    $('#bart').text(`${station.name} to ${station.etd[0].destination}`)
+    departures.forEach(departure => {
+        $('#depart').append(`<li>${departure.minutes}</li>`)
+    });
+ }
+
+const errorLog = (err,err2,err3) =>{
+    console.log(err2);
+}
+
+setInterval(() => { 
+     getStation();
+  }  , 60000 );
+
+const getStation = () => {
+    $.ajax({
+        method: 'GET',
+        url: bartApi,
+        success: bartSucc,
+        error: errorLog
+     });
+}
+
+getStation();
